fd2d525ea9a56c73679eff72f909130ddd554f21
mike seiler <snissn@gmail.com>
2022-11-08 14:25:12 -1000

    attempt to restyle the doc item for full wdth

---
+++ src/theme/DocItem/Layout/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@theme-original/DocItem/Layout';
+import '@site/static/style.css'
+
+export default function LayoutWrapper(props) {
+  return (
+    <>
+      <Layout {...props} />
+    </>
+  );
+}
