37bb78a98cceb126b2bc51504b96477d9be7c9e5
mike seiler <snissn@gmail.com>
2022-11-08 14:08:16 -1000

    style to make container full width if theres no sidebar

---
+++ src/theme/Root.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import '@site/static/style.css'
+
+// Default implementation, that you can customize
+export default function Root({children}) {
+  return <>{children}</>;
+}
