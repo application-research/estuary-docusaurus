e716994fd79e7d091df93b794fb1fda6090268ce
mike seiler <snissn@gmail.com>
2022-11-14 06:32:23 -1000

    update get api key tutorial

--- legacy/components/RichText.tsx
+++ legacy/components/RichText.tsx
@@ -1,22 +1,13 @@
-import styles from '@components/RichText.module.scss';
+import styles from '@site/legacy/components/RichText.module.scss';
 
 import * as React from 'react';
-import * as U from '@common/utilities';
+import * as U from '@site/legacy/common/utilities';
 
 import { Remarkable } from 'remarkable';
 import RemarkableReactRenderer from 'remarkable-react';
 
 export default function RichText(props) {
-  const md = new Remarkable({
-    linkTarget: '_blank',
-  });
-
-  const markdown = md.render(props.children);
-
   return (
-    <div
-      className={U.classNames(styles.block, styles.container)}
-      dangerouslySetInnerHTML={{ __html: markdown }}
-    />
+{props.children}
   );
 }
