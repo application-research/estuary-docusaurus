8b359434ce35ee91c65531165c21d8c31c536991
mike seiler <snissn@gmail.com>
2022-11-21 14:21:03 -1000

    add additional sed command that replaces random className label with the actual class name

--- refresh-client-docs/run.sh
+++ refresh-client-docs/run.sh
@@ -17,9 +17,10 @@ for folder in $folders; do
   find "$folder" -type f | grep '.md$' | grep -v node_module| while IFS=$'\n' read -r filename; do
     destination_folder=$(dirname "$SCRIPT_DIR/../clients/$filename")
     destination_filename="$SCRIPT_DIR/../clients/$filename"
+    prefix_filename="$(basename "${filename%.*}")"
     mkdir -p "$destination_folder"
     cp -v "$filename" "$destination_folder"
-    sed -i ''  -e 's/^# \*\*\([0-9a-zA-Z_]*\)\*\*/## **\1** {#\1}/g' -e 's/<a\ name=\".*//g' "$destination_filename" 
+    sed -i '' -e 's/^# \*\*\([0-9a-zA-Z_]*\)\*\*/## **\1** {#\1}/g' -e 's/<a\ name=\".*//g' -e "s/# {{classname}}/# $prefix_filename/" "$destination_filename"
   done
 
 
